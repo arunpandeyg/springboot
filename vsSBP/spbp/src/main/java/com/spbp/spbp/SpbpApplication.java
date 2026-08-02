f55b69f4efd03e82d2e0b43e531d1c7bcf4e47a4
Arun Pandey <128284476+arunpandeyg@users.noreply.github.com>
2026-08-02 19:13:02 +0530

    Add files via upload

---
+++ vsSBP/spbp/src/main/java/com/spbp/spbp/SpbpApplication.java
@@ -0,0 +1,14 @@
+package com.spbp.spbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbpApplication.class, args);
+		
+	}
+
+}
---
+++ vsSBP/spbp/src/test/java/com/spbp/spbp/SpbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spbp.spbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
